26c3947850c24b863765d8ba3c5ed17fb5a1a386
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2019-10-24 14:23:57 +0000

    Overhaul map initialization to get metadata first

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,136 +1,97 @@
 "use strict";
 
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/micahbf/cjwp89saq75s21cpjhh9126sr",
-  center: [-122.206582, 39.215639],
-  zoom: 5
-});
+const mapDataParams = "from=2019-09-30T00:00:00.000Z";
 
-map.addControl(new mapboxgl.FullscreenControl());
+fetch(`https://mapapp.micah.motorcycles/map_metadata?${mapDataParams}`).then((response) => {
+  const metadata = response.json();
 
-map.on("load", () => {
-  const mapDataUrl = "https://mapapp.micah.motorcycles/map_data?from=2019-09-30T00:00:00.000Z";
-  const timeZone = 'America/Mexico_City';
-  const initMapBoundTime = 24 * 60 * 60 * 1000;
+  const captionEl = document.getElementById("map-last-updated");
+  captionEl.innerHTML = metadata.updateCaption;
 
-  map.addSource("mapData", { type: "geojson", data: mapDataUrl });
-
-  map.addLayer({
-    id: "liveTrack",
-    type: "line",
-    source: "mapData",
-    filter: ["==", ["get", "class"], "liveTrack"],
-    layout: {
-      "line-join": "round",
-      "line-cap": "round"
-    },
-    paint: {
-      "line-color": "#600000",
-      "line-width": 5
-    }
-  }, "poi-label");
-
-  map.addLayer({
-    id: "endOfDays",
-    type: "symbol",
-    source: "mapData",
-    filter: ["==", ["get", "class"], "endOfDay"],
-    layout: {
-      "icon-image": "circle-11"
-    }
+  return new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/micahbf/cjwp89saq75s21cpjhh9126sr",
+    bounds: metadata.bounds,
+    fitBoundOptions: {padding: 30, maxZoom: 5}
   });
-
-  map.addLayer({
-    id: "lastUpdate",
-    type: "symbol",
-    source: "mapData",
-    filter: ["==", ["get", "class"], "lastUpdate"],
-    layout: {
-      "icon-image": "star-stroked-11"
-    }
+}).catch(() => {
+  return new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/micahbf/cjwp89saq75s21cpjhh9126sr",
+    center: [-122.206582, 39.215639],
+    zoom: 5
   });
+}).then((map) => {
+  map.addControl(new mapboxgl.FullscreenControl());
 
-  const popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
-  });
+  map.on("load", () => {
+    const mapDataUrl = `https://mapapp.micah.motorcycles/map_data?${mapDataParams}`;
+    const timeZone = 'America/Mexico_City';
 
-  const addPointPopup = (e) => {
-    map.getCanvas().style.cursor = "pointer";
+    map.addSource("mapData", { type: "geojson", data: mapDataUrl });
 
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    const name = e.features[0].properties.name;
+    map.addLayer({
+      id: "liveTrack",
+      type: "line",
+      source: "mapData",
+      filter: ["==", ["get", "class"], "liveTrack"],
+      layout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      paint: {
+        "line-color": "#600000",
+        "line-width": 5
+      }
+    }, "poi-label");
 
-    popup.setLngLat(coordinates)
-      .setHTML(name)
-      .addTo(map);
-  };
-
-  const removePointPopup = () => {
-    map.getCanvas().style.cursor = "";
-    popup.remove();
-  };
-
-  const getLastUpdate = () => {
-    return map.querySourceFeatures(
-      "mapData",
-      {filter: ["==", ["get", "class"], "lastUpdate"]}
-    )[0];
-  };
-
-  const setCaptionFromLastUpdate = () => {
-    const lastUpdateObj = getLastUpdate();
-    if(!lastUpdateObj) { return; };
-
-    const captionText = lastUpdateObj.properties.name;
-    const captionEl = document.getElementById("map-last-updated");
-    captionEl.innerHTML = captionText;
-  };
-
-  const setBoundsFromMapData = () => {
-    const lastUpdateObj = getLastUpdate();
-    if(!lastUpdateObj) { return false; };
-
-    const lastUpdateDate = new Date(lastUpdateObj.properties.isoTime);
-
-    const endOfDays = map.querySourceFeatures(
-      "mapData",
-      {filter: ["==", ["get", "class"], "endOfDay"]}
-    );
-
-    const byTimeDesc = endOfDays.sort(
-      (a, b) => b.properties.isoTime.localeCompare(a.properties.isoTime)
-    );
-
-    const targetBound = byTimeDesc.find((endOfDay) => {
-      const date = new Date(endOfDay.properties.isoTime);
-      return lastUpdateDate - date > initMapBoundTime;
+    map.addLayer({
+      id: "endOfDays",
+      type: "symbol",
+      source: "mapData",
+      filter: ["==", ["get", "class"], "endOfDay"],
+      layout: {
+        "icon-image": "circle-11"
+      }
     });
-    if(!targetBound) { return false; };
 
-    const bounds = [
-      lastUpdateObj.geometry.coordinates,
-      targetBound.geometry.coordinates
-    ];
+    map.addLayer({
+      id: "lastUpdate",
+      type: "symbol",
+      source: "mapData",
+      filter: ["==", ["get", "class"], "lastUpdate"],
+      layout: {
+        "icon-image": "star-stroked-11"
+      }
+    });
 
-    map.fitBounds(bounds, {padding: 30, maxZoom: 5});
-    return true;
-  };
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
 
-  map.on("mouseenter", "endOfDays", addPointPopup);
-  map.on("mouseleave", "endOfDays", removePointPopup);
+    const addPointPopup = (e) => {
+      map.getCanvas().style.cursor = "pointer";
 
-  map.on("mouseenter", "lastUpdate", addPointPopup);
-  map.on("mouseleave", "lastUpdate", removePointPopup);
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const name = e.features[0].properties.name;
 
-  let initBoundsSet = false;
-  map.on("sourcedata", (mapDataEvent) => {
-    if(mapDataEvent.sourceId !== "mapData") { return; };
-    setCaptionFromLastUpdate();
-    if(!initBoundsSet) {
-      initBoundsSet = setBoundsFromMapData();
-    }
+      popup.setLngLat(coordinates)
+        .setHTML(name)
+        .addTo(map);
+    };
+
+    const removePointPopup = () => {
+      map.getCanvas().style.cursor = "";
+      popup.remove();
+    };
+
+    map.on("mouseenter", "endOfDays", addPointPopup);
+    map.on("mouseleave", "endOfDays", removePointPopup);
+
+    map.on("mouseenter", "lastUpdate", addPointPopup);
+    map.on("mouseleave", "lastUpdate", removePointPopup);
   });
-
 });
+
+
